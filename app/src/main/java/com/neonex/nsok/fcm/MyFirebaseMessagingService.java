98c2b790a9c6cd2d68fe17a6e7adb36469dbdac5
yunseungkim <yun87k@neonexsoft.com>
2018-03-22 14:11:20 +0900

    mj

--- app/src/main/java/com/neonex/nsok/fcm/MyFirebaseMessagingService.java
+++ app/src/main/java/com/neonex/nsok/fcm/MyFirebaseMessagingService.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.media.RingtoneManager;
 import android.net.Uri;
+import android.os.PowerManager;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.NotificationCompat;
 
@@ -29,7 +30,7 @@ import java.util.Map;
  */
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
-
+    private static PowerManager.WakeLock mCpuWakeLock = null;
     private static final String TAG = "MyFirebaseMessagingService";
 
     @Override
@@ -53,9 +54,18 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     private void sendPushNotification(RemoteMessage remoteMessage) {
         // EventBus 를 통해서 넘어온 데이터를 액티비티로 보낸다 (화면 액션에 필요)
         EventBus.getDefault().postSticky(new ReceiverEvent(remoteMessage.getData().get("targetUrl").toString()));
+        PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+
+        mCpuWakeLock = pm.newWakeLock(
+                PowerManager.SCREEN_BRIGHT_WAKE_LOCK |
+                        PowerManager.ACQUIRE_CAUSES_WAKEUP |
+                        PowerManager.ON_AFTER_RELEASE, "hi");
+
+        mCpuWakeLock.acquire();
 
         Intent intent = new Intent(this, MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        //
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
                 PendingIntent.FLAG_ONE_SHOT);
 
@@ -64,8 +74,8 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         android.support.v4.app.NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
                 .setSmallIcon(R.drawable.ic_notification_small)
                 .setPriority(Notification.PRIORITY_MAX)
-                .setContentTitle(remoteMessage.getNotification().getTitle().toString())
-                .setContentText(remoteMessage.getNotification().getBody().toString())
+                .setContentTitle(remoteMessage.getData().get("title"))
+                .setContentText(remoteMessage.getData().get("body"))
                 .setAutoCancel(true)
                 .setColor(ContextCompat.getColor(this, R.color.statusBarColor))
                 .setSound(defaultSoundUri).setLights(000000255, 500, 2000)
